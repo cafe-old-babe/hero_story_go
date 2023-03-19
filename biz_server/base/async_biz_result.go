f344817acfe2dea7401c691f49acfdfb10116099
Leo <hanss@mvtech.com.cn>
2023-03-20 00:26:14 +0800

    todo

--- biz_server/base/async_biz_result.go
+++ biz_server/base/async_biz_result.go
@@ -2,7 +2,7 @@ package base
 
 type AsyncBizResult struct {
 	returnObj interface{}
-	// 1. 如果不在continueWith上调用的话,会有多线程问题读写的问题,完成函数跨线程(携程)了,2.会有不被调用的风险,赋值与调用错位,3.多次调用
+	// todo 1. 如果不在continueWith上调用的话,会有多线程问题读写的问题,完成函数跨线程(携程)了,2.会有不被调用的风险,赋值与调用错位,3.多次调用
 	completeFunc func()
 }
 

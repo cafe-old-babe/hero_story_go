3ecaa48bfdbd59d27946881a8db42e6f5c5a1b30
Leo <hanss@mvtech.com.cn>
2023-03-26 00:58:19 +0800

    first commit

---
+++ gateway_server/gateway_server.go
@@ -0,0 +1,55 @@
+package gateway_server
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"hero_story/comm/log"
+	"net/http"
+	"os"
+	"path"
+)
+
+var sessionId int32 = 0
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  2048,
+	WriteBufferSize: 2048,
+	//跨域
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	fmt.Println("start gatewayServer")
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Config(path.Dir(ex) + "/log/gateway_server.log")
+	log.Info("bizServer start success")
+
+	http.HandleFunc("/websocket", websocketHandShake)
+
+	_ = http.ListenAndServe(":54321", nil)
+
+}
+
+func websocketHandShake(w http.ResponseWriter, r *http.Request) {
+	if nil == w || nil == r {
+		return
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Error("websocket upgrade error: %v", err)
+	}
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Error("websocket close error: %v", err)
+		}
+	}()
+	log.Info("有新客户端加入")
+	sessionId += 1
+
+}
---
+++ gateway_server/network/websocket/cmd_context_impl.go
@@ -0,0 +1,132 @@
+package websocket
+
+import (
+	"encoding/binary"
+	"github.com/gorilla/websocket"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"hero_story/biz_server/msg"
+	"hero_story/comm/log"
+	"time"
+)
+
+const oneSecondMilli = 1000
+const limitOnceByByteCount = 64 * 1024
+const limitByPacketCountPerSecond = 16
+
+// CmdContextImpl Implemented on MyCmdContext
+type CmdContextImpl struct {
+	userId       int64
+	clientIpAddr string
+	Conn         *websocket.Conn
+	sendMsgQueue chan *protoreflect.ProtoMessage
+	SessionId    int32
+}
+
+// BindUserId 绑定用户
+func (ctx *CmdContextImpl) BindUserId(val int64) {
+	ctx.userId = val
+}
+
+// GetUserId 获取用户
+func (ctx *CmdContextImpl) GetUserId() int64 {
+	return ctx.userId
+}
+
+// GetClientIpAddr 获取客户端ip
+func (ctx *CmdContextImpl) GetClientIpAddr() string {
+	return ctx.clientIpAddr
+}
+
+// Write 写入消息
+func (ctx *CmdContextImpl) Write(msgObj protoreflect.ProtoMessage) {
+	if nil == msgObj || ctx.Conn == nil || ctx.sendMsgQueue == nil {
+		return
+	}
+	ctx.sendMsgQueue <- &msgObj
+
+}
+
+// SendError 错误消息
+func (ctx *CmdContextImpl) SendError(errorCode int, errorInfo string) {
+
+}
+
+// Disconnect 断开连接
+func (ctx *CmdContextImpl) Disconnect() {
+	if ctx == nil || ctx.Conn == nil {
+
+		return
+	}
+	_ = ctx.Conn.Close()
+}
+
+// LoopSendMsg 发送消息
+func (ctx *CmdContextImpl) LoopSendMsg() {
+	if ctx.sendMsgQueue != nil {
+		return
+	}
+	ctx.sendMsgQueue = make(chan *protoreflect.ProtoMessage, 1024)
+	for {
+		msgObj := <-ctx.sendMsgQueue
+		if msgObj == nil {
+			continue
+		}
+		byteArray, err := msg.Encode(msgObj)
+		if err != nil {
+			log.Error("[websocket] Encode msg error: %v", err)
+			return
+		}
+		if err := ctx.Conn.WriteMessage(websocket.BinaryMessage, byteArray); err != nil {
+			log.Error("[websocket] WriteMessage error: %+v", err)
+		}
+	}
+}
+
+// LoopReadMsg 读取消息
+func (ctx *CmdContextImpl) LoopReadMsg() {
+	if nil == ctx.Conn {
+		return
+	}
+	ctx.Conn.SetReadLimit(limitOnceByByteCount)
+	//limit the count of packet
+	t0 := int64(0)
+	counter := 0
+	for {
+		func() {
+			defer func() {
+				if e := recover(); e != nil {
+					log.Error("解析消息出错：%+v", e)
+				}
+				_, msgData, err := ctx.Conn.ReadMessage()
+				if err != nil {
+					log.Error("websocket readMessage error: %v ", err)
+					return
+				}
+				log.Info("msgData: %v", msgData)
+
+				t1 := time.Now().UnixMilli()
+				if (t1 - t0) > oneSecondMilli {
+					t0 = t1
+					counter = 0
+				}
+
+				if counter >= limitByPacketCountPerSecond {
+					log.Error("消息过于频繁, 拒绝处理, userId: %v, clientIp: %s", ctx.GetUserId(), ctx.GetClientIpAddr())
+					return
+				}
+				counter++
+
+				msgCode := binary.BigEndian.Uint16(msgData[2:4])
+				message, err := msg.Decode(msgData[4:], int16(msgCode))
+				if err != nil {
+					log.Error("message message msgCode: %d, err: %+v", msgCode, err)
+					return
+				}
+				log.Info("收到客户端消息,msgCode: %d, message Name: %v", msgCode, message.Descriptor().Name())
+
+			}()
+		}()
+
+	}
+
+}

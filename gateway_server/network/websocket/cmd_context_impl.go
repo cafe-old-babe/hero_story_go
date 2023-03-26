d21a202c17268ae00cb5efb9b142fd4803c229a7
Leo <hanss@mvtech.com.cn>
2023-03-26 23:36:38 +0800

    创建网关服务器

--- gateway_server/network/websocket/cmd_context_impl.go
+++ gateway_server/network/websocket/cmd_context_impl.go
@@ -94,31 +94,55 @@ func (ctx *CmdContextImpl) LoopReadMsg() {
 	//limit the count of packet
 	t0 := int64(0)
 	counter := 0
+	//创建游戏服务器连接
+	bizServerConn, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:12345/websocket", nil)
+	if nil != err {
+		log.Error("[websocket] Dial error: %+v", err)
+		return
+	}
+	// region 循环读取游戏服发来的消息,转发给客户端
+	go func() {
+		for {
+			//读取游戏服务器返回的数据
+			msgType, msgData, err := bizServerConn.ReadMessage()
+			if err != nil {
+				log.Error("从服务器读取消息失败: %+v", err)
+			}
+			//ctx.Conn 网关服务器到游戏客户端连接
+			if err = ctx.Conn.WriteMessage(msgType, msgData); nil != err {
+				log.Error("网关服务器到发送消息失败: %+v", err)
+			}
+		}
+	}()
+	//endregion
+
+	// region 循环读取游戏客户端发了的消息,转发给游戏服
 	for {
+		// 接收游戏客户端消息
+		messageType, msgData, err := ctx.Conn.ReadMessage()
+		if err != nil {
+			log.Error("websocket readMessage error: %v ", err)
+			return
+		}
+		log.Info("msgData: %v", msgData)
+
+		t1 := time.Now().UnixMilli()
+		if (t1 - t0) > oneSecondMilli {
+			t0 = t1
+			counter = 0
+		}
+
+		if counter >= limitByPacketCountPerSecond {
+			log.Error("消息过于频繁, 拒绝处理, userId: %v, clientIp: %s", ctx.GetUserId(), ctx.GetClientIpAddr())
+			return
+		}
+		counter++
 		func() {
 			defer func() {
 				if e := recover(); e != nil {
 					log.Error("解析消息出错：%+v", e)
 				}
 			}()
-			_, msgData, err := ctx.Conn.ReadMessage()
-			if err != nil {
-				log.Error("websocket readMessage error: %v ", err)
-				return
-			}
-			log.Info("msgData: %v", msgData)
-
-			t1 := time.Now().UnixMilli()
-			if (t1 - t0) > oneSecondMilli {
-				t0 = t1
-				counter = 0
-			}
-
-			if counter >= limitByPacketCountPerSecond {
-				log.Error("消息过于频繁, 拒绝处理, userId: %v, clientIp: %s", ctx.GetUserId(), ctx.GetClientIpAddr())
-				return
-			}
-			counter++
 
 			msgCode := binary.BigEndian.Uint16(msgData[2:4])
 			message, err := msg.Decode(msgData[4:], int16(msgCode))
@@ -126,10 +150,14 @@ func (ctx *CmdContextImpl) LoopReadMsg() {
 				log.Error("message message msgCode: %d, err: %+v", msgCode, err)
 				return
 			}
-			log.Info("收到客户端消息,msgCode: %d, message Name: %v", msgCode, message.Descriptor().Name())
-
+			log.Info("收到客户端消息 并转发,msgCode: %d, message Name: %v", msgCode, message.Descriptor().Name())
+			//向socket服务端发送消息
+			//转发给游戏服
+			if err = bizServerConn.WriteMessage(messageType, msgData); nil != err {
+				log.Error("转发消息失败, err: %+v", err)
+			}
 		}()
 
 	}
-
+	// endregion
 }
